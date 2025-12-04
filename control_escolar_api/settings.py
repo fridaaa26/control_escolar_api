3369a7c6b753d3936d1bbe1d517c67ef69eed1b3
fridaaa26 <fridalop264@gmail.com>
2025-12-03 21:24:33 -0600

    Fix ALLOWED_HOSTS for Render

--- control_escolar_api/settings.py
+++ control_escolar_api/settings.py
@@ -8,7 +8,12 @@ SECRET_KEY = '-_&+lsebec(whhw!%n@ww&1j=4-^j_if9x8$q778+99oz&!ms2'
 
 DEBUG = True  # en desarrollo
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'control-escolar-api-uq4t.onrender.com'
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
